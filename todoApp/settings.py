2f07815e5fa5f9807497fbd579ec7ad3dd34fb4d
konark111 <136243900+konark111@users.noreply.github.com>
2023-06-28 17:05:54 +0530

    Update settings.py 1.1

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,3 +127,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
